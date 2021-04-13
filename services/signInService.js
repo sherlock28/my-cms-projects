f8f352333a030309eaeaae1f40551d344b99e88d
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-04-13 01:08:11 -0300

    Sign in service is added

--- services/signInService.js
+++ services/signInService.js
@@ -1,3 +1,22 @@
+import { API_URL } from "./settings";
+
 export function signInService({ email, password }) {
-  console.log(email, password);
+  let isLoggedOk = false;
+  let jwt = "";
+
+  return fetch(`${API_URL}/users/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then(response => {
+      jwt = response.headers.get("Authorization");
+      return response.json();
+    })
+    .then(res => {
+      res.status === "Ok" ? (isLoggedOk = true) : (isLoggedOk = false);
+      return { message: res.message, isLoggedOk, jwt };
+    });
 }
