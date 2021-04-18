ea4e06427e522cd24d3c5fc1eda44d5d3f5b5457
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-04-18 06:34:33 -0300

    Scripts bootstrap is added

---
+++ components/HeadContent/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HeadContent() {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,24 @@
+import Head from "next/head";
 import "../styles/globals.css";
 import "bootswatch/dist/materia/bootstrap.min.css";
 import "animate.css/animate.min.css";
 import { UserContextProvider } from "context/UserContext";
+import HeadContent from "components/HeadContent";
+import Scripts from "components/Scripts";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <UserContextProvider>
-      <Component {...pageProps} />
-    </UserContextProvider>
+    <>
+      <Head>
+        <HeadContent />
+      </Head>
+
+      <UserContextProvider>
+        <Component {...pageProps} />
+      </UserContextProvider>
+      
+      <Scripts />
+    </>
   );
 }
 
